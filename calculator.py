03aa29d2ace0a92487c94d6c92bc90660011a1d0
Michael O'Rourke <Lemonhead@localhost.localdomain>
2021-12-05 01:30:09 +0000

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -1,17 +1,12 @@
 """
-Unit tests for the calculator library
+Calculator library containing basic math operations.
 """
 
-import calculator
+def add(first_term, second_term):
+    return first_term + second_term
 
+def subtract(first_term, second_term):
+    return first_term - second_term
 
-class TestCalculator:
-
-    def test_addition(self):
-        assert 4 == calculator.add(2, 2)
-
-    def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
-
-    def multiply(first_term, second_term):
-        return first_term * second_term
+def multiply(first_term, second_term):
+    return first_term * second_term
