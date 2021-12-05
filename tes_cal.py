fe914bf151158d0c1fcf9d19f68c4a1cd0c773c2
Michael O'Rourke <Lemonhead@localhost.localdomain>
2021-12-05 01:52:20 +0000

    Add unit tests for calculator

--- tes_cal.py
+++ tes_cal.py
@@ -9,14 +9,11 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
 
-
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-
 
     def test_divsion(self):
         assert 2 == calculator.divsion(10, 5)
